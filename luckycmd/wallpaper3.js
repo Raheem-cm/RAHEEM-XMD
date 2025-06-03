51747f9cea33e5fa53851bbf8949e8de7231eb4d
Raheem-cm <nyoniraheem@gmail.com>
2025-06-03 23:15:05 +0300

    Update wallpaper3.js

--- luckycmd/wallpaper3.js
+++ luckycmd/wallpaper3.js
@@ -4,9 +4,9 @@ Object.defineProperty(exports, "__esModule", {
   'value': true
 });
 const {
-  ezra
-} = require("../Raheem-cm/18");
-ezra({
+  Raheem
+} = require("../Raheem-cm/Raheem");
+Raheem({
   'nomCom': "wallpaper3",
   'reaction': '🎞',
   'nomFichier': __filename
